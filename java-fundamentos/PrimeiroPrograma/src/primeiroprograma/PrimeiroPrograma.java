4e89509a7120ff771ad78b01b353ade410867c36
marcelpinotti <marcel.pinotti@gmail.com>
2021-01-10 21:51:10 -0300

    Iniciando Repositório

---
+++ java-fundamentos/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package primeiroprograma;
+
+//@author Marcel Pinotti
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!!!!");
+        System.out.print("Olá MUNDO!!!!");
+    }
+    
+}
