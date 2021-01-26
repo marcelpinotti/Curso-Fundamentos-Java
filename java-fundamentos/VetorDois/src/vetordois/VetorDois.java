8620b5353d101d343be11f32d2b16b236a502d66
marcelpinotti <marcel.pinotti@gmail.com>
2021-01-25 21:50:38 -0300

    Inciando Vetores

---
+++ java-fundamentos/VetorDois/src/vetordois/VetorDois.java
@@ -0,0 +1,41 @@
+package vetordois;
+
+import java.util.Scanner;
+
+/**
+ * @author Marcel Pinotti
+ */
+public class VetorDois {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite o ano atual: ");
+        int anoAtual = teclado.nextInt();
+        
+        if (anoAtual % 4 == 0 || anoAtual % 400 == 0 && anoAtual % 100 != 0) {
+            System.out.println("É um ano Bisexto!!!");
+
+            String meses[] = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun",
+                "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
+            int tot[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+            for (int c = 0; c < meses.length; c++) {
+                System.out.println("O mês de " + meses[c] + " tem " + tot[c] + " dias ao todo.");
+            }
+
+        } else {
+            System.out.println("Não é um ano Bisexto!!!");
+            String meses[] = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun",
+                "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
+            int tot[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+            for (int c = 0; c < meses.length; c++) {
+                System.out.println("O mês de " + meses[c] + " tem " + tot[c] + " dias ao todo.");
+            }
+        }
+
+        teclado.close();
+    }
+    
+}
---
+++ java-fundamentos/VetorTres/src/vetortres/VetorTres.java
@@ -0,0 +1,17 @@
+package vetortres;
+/**
+ * @author Marcel Pinotti
+ */
+public class VetorTres {
+
+    public static void main(String[] args) {
+        String meses[] = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun",
+            "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
+        int tot[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        for (int c = 0; c < meses.length; c++){
+        System.out.println("O mês de " + meses[c] + " tem " + tot[c] + " dias ao todo.");
+        }
+    }
+    
+}
